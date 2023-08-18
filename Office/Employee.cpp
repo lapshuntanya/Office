399ceaa1761464711ef76e017c21732a9c8afc92
Lapshun Tetiana <lapshun.tatiana@gmail.com>
2023-08-18 20:24:08 +0300

    реализация класса Сотрудник (Employee.cpp)

---
+++ Office/Employee.cpp
@@ -0,0 +1,24 @@
+#include "Employee.h"
+
+Employee::Employee()
+{
+    name = "Noname";
+    position = "Noposition";
+    salary = 0;
+}
+
+Employee::Employee(string name, string position, float salary){
+    this->name = name;
+    this->position = position;
+    this->salary = salary;
+}
+
+void Employee::show() const{
+    cout << "Name: " << name << endl;
+    cout << "Position: " << position << endl;
+    cout << "Salary: " << salary << "uah" << endl;
+}
+
+float Employee::calcSalary() const{
+    return salary;
+}
